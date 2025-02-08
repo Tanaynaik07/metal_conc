78208d6f0cf7422359408aad31477000782237e8
Tanay <110788436+Tanaynaik07@users.noreply.github.com>
2025-02-08 17:51:58 +0530

    Update index.js (changed values)

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ function update() {
     const result = document.getElementById('result');
 
     // Determine the resulting color description based on input values
-    if (red <= 50 && red>=48  && green>=41 && green <= 42 && blue>=57 &&  blue <= 58) {
+    if (red <= 47 && red>=51  && green>=39 && green <= 43 && blue>=55 &&  blue <= 60) {
         result.innerText = "Concentration: 0ppm";
-    } else if (red>=45 && red <= 46  && green>=35 && green <= 36 && blue>=49 &&  blue <= 50) {
+    } else if (red>=40 && red <= 43  && green>=32 && green <= 34 && blue>=46 &&  blue <= 48) {
         result.innerText = "Concentration: 2ppm";
-    } else if (red>=46 && red <= 48  && green>=35 && green <= 36 && blue>=49 &&  blue <= 50) {
+    } else if (red>=44 && red <= 47  && green>=34 && green <= 37 && blue>=49 &&  blue <= 52) {
         result.innerText = "Concentration: 4ppm";
-    } else if (red>=44 && red <= 45  && green>=32 && green <= 32 && blue>=45 &&  blue <= 46) {
+    } else if (red>=43 && red <= 51  && green>=34 && green <= 41 && blue>=48 &&  blue <= 56) {
         result.innerText = "Concentration: 6ppm";
-    } else if (red>=45 && red <= 46  && green>=33 && green <= 33 && blue>=46 &&  blue <= 46) {
+    } else if (red>=41 && red <= 48  && green>=32 && green <= 38 && blue>=45 &&  blue <= 53) {
         result.innerText = "Concentration: 8ppm";
-    } else if (red>=37 && red <= 38  && green>=24 && green <= 25 && blue>=37 &&  blue <= 38) {
+    } else if (red>=39 && red <= 41  && green>=28 && green <= 31 && blue>=40 &&  blue <= 43) {
         result.innerText = "Concentration: 10ppm";
     }  else {
         result.innerText = "Concentration: Off the Chart";
